c712951112e739f3599aaf35105608959c61813c
cvidos <catherine.vidos@gmail.com>
2020-09-21 18:01:28 -0400

    stuff

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+
+const App = () => (
+  <div>
+    <h1>I'm an app</h1>
+  </div>
+);
+
+export default App;
