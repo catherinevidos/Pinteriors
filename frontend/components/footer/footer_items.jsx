2d8e550888e7813fd391843ff4a82656b244e11e
cvidos <catherine.vidos@gmail.com>
2020-09-24 14:20:54 -0400

    fixing styling

--- frontend/components/footer/footer_items.jsx
+++ frontend/components/footer/footer_items.jsx
@@ -1,19 +1,25 @@
 export const FooterItems = [
   {
     id: 1,
+    title: 'Made with 💖 by Catherine Vidos',
+    url: 'https://github.com/catherinevidos',
+    cName: 'footer-link-emoji'
+  },
+  {
+    id: 2,
     title: 'Github',
     url: '#',
     cName: 'footer-link'
   },
   {
-    id: 2,
-    title: 'Link to something else',
+    id: 3,
+    title: 'Link',
     url: '#',
     cName: 'footer-link'
   },
   {
-    id: 3,
-    title: 'Link to something else',
+    id: 4,
+    title: 'Link',
     url: '#',
     cName: 'footer-link'
 
